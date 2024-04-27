a23f5f3bf30a838aecbf8cb5945687c31a3ff812
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-27 17:20:48 -0400

    Added constants, hooks, types, lib and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import Room from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
-  return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
-  );
+  return <h1 className="text-5xl text-white">Hi</h1>;
 }
